3271b106784de52df2dc53754efe1b82ada4243a
Rosalie Tolentino <rtolenti@thoughtworks.com>
2017-06-29 16:35:58 -0500

    Adds changes to code

--- arduino_code/tigerkey/tigerkey.ino
+++ arduino_code/tigerkey/tigerkey.ino
@@ -1,16 +1,17 @@
 #include "DigiKeyboard.h"
-int pb2 = 2;
+int pb3 = 3;
 int pb4 = 4;
 
 void setup() {
-  pinMode(pb2, INPUT);
-  digitalWrite(pb2, HIGH); // set pull up circuit
+  pinMode(pb3, INPUT);
+  digitalWrite(pb3, HIGH); // set pull up circuit
   pinMode(pb4, OUTPUT);
   digitalWrite(pb4, HIGH);
+  DigiKeyboard.sendKeyStroke(0);
 }
 
 void loop() {
-  if (digitalRead(pb2) == LOW) {
+  if (digitalRead(pb3) == LOW) {
     DigiKeyboard.println("Your Password Here");    
   }
 }
